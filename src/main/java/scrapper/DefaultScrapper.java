08480b5579f362498024af53162b6a385028a54b
booyakashakawabangha <tatarik.sv@gmail.com>
2021-12-23 12:18:12 +0200

    Improved methods in DefaultScrapper

--- src/main/java/scrapper/DefaultScrapper.java
+++ src/main/java/scrapper/DefaultScrapper.java
@@ -6,16 +6,39 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public class DefaultScrapper implements Scrapper{
+    private static final String PRICE_SELECTOR = ".detail__info-xlrg";
+    private static final String BED_SELECTOR = ".nhs_BedsInfo";
+    private static final String BATH_SELECTOR = ".detail__info-xlrg";
+    private static final String GARAGE_SELECTOR = ".detail__info-xlrg";
 
     @Override @SneakyThrows
     public Home parse(String url) {
         Document doc = Jsoup.connect(url).get();
-        Element priceTag = doc.selectFirst("detail__info-xlrg");
-        Element bedTag = doc.selectFirst("nhs_BedsInfo");
-        Element bathTag = doc.selectFirst("nhs_BedsInfo");
-        Element garageTag = doc.selectFirst("nhs_BedsInfo");
-        System.out.println(doc);
+        int price = getPrice(doc);
+        double beds = getBeds(doc);
+        double bathes = getBathes(doc);
+        double garages = getGarages(doc);
+        return new Home(price, beds, bathes, garages);
+    }
 
-        return null;
+    private int getPrice(Document doc) {
+        String price = doc.selectFirst(PRICE_SELECTOR).text().replaceAll("[^0-9]", "");
+        return Integer.parseInt(price);
+    }
+
+    private double getBeds(Document doc) {
+        String beds = doc.selectFirst(BED_SELECTOR).text().replaceAll("[^.0-9]", "");
+        return Double.parseDouble(beds);
+    }
+
+    private double getBathes(Document doc) {
+        String bathes = doc.selectFirst(BATH_SELECTOR).text().replaceAll("[^.0-9]", "");
+        return Double.parseDouble(bathes);
+    }
+
+    private double getGarages(Document doc) {
+        String garages = doc.selectFirst(GARAGE_SELECTOR).text().replaceAll("[^.0-9]", "");
+        return Double.parseDouble(garages);
     }
 }
+
