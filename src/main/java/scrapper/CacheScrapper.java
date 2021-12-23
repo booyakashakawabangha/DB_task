71c86c4d16fe6f3cd5b07b568b5569fd7bbe3741
booyakashakawabangha <tatarik.sv@gmail.com>
2021-12-23 14:21:34 +0200

    Refactored

--- src/main/java/scrapper/CacheScrapper.java
+++ src/main/java/scrapper/CacheScrapper.java
@@ -4,12 +4,19 @@ import lombok.SneakyThrows;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class CacheScrapper implements Scrapper{
 
     @Override @SneakyThrows
     public Home parse(String url) {
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:db.sqlite");
+        Statement statement = connection.createStatement();
+
+        String query = String.format("select count(*) as count from homes where url='%s'", url);
+        ResultSet rs = statement.executeQuery(query);
+
         return null;
     }
 }
